2174f37696bf4549a84149b146eb3aa083c5de62
LionFang0620 <yiweifnag@gmail.com>
2025-09-29 21:12:58 +0800

    新增商店功能第一版

--- 原始檔案/dao/StoreDAO.java
+++ 原始檔案/dao/StoreDAO.java
@@ -10,16 +10,40 @@ import javax.persistence.TypedQuery;
 import entity.Store;
 
 public class StoreDAO {
-	EntityManager createConnection() {
+	EntityManager createConnection() 
+	{
     	EntityManagerFactory factory=Persistence.createEntityManagerFactory("easybuyfarm");
     	return factory.createEntityManager();
     }
 	
-	 public List<Store> getAllStore(){
+	 public List<Store> getAllStore()
+	 {
 	    	
 	    	EntityManager mgr=createConnection();
 	    	TypedQuery<Store> data= mgr.createNamedQuery("Store.findAll", Store.class);
 	    	
 	    	return data.getResultList();
-	    }
+	 }
+	 
+	 public boolean addStore(Store store)
+	 {
+		 EntityManager mgr=createConnection();
+		 try
+		 {
+			 mgr.getTransaction().begin();
+			 mgr.persist(store);
+			 mgr.getTransaction().commit();
+			 return true;
+		 }
+		 
+		 catch(Exception ex)
+		 {
+			 System.out.println("Add Store Error "+ex.getMessage());
+		 }
+		 finally 
+		 {
+			 mgr.close();
+		 }
+		 return false ;
+	 }
 }
