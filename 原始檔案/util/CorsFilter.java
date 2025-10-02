de07585e624e956bd00f79a2329404e8eeacc16e
LionFang0620 <yiweifnag@gmail.com>
2025-10-02 23:01:07 +0800

    新增允許跨網域接受session&直接指定前端網域

--- 原始檔案/util/CorsFilter.java
+++ 原始檔案/util/CorsFilter.java
@@ -12,10 +12,12 @@ public class CorsFilter implements Filter {
 			throws IOException, ServletException {
 		HttpServletResponse response = (HttpServletResponse) res;
 		HttpServletRequest request = (HttpServletRequest) req;
-		response.setHeader("Access-Control-Allow-Origin", "*");
+		response.setHeader("Access-Control-Allow-Origin", "http://127.0.0.1:5500");
+		response.setHeader("Access-Control-Allow-Credentials", "true");
 		response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
 		response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 		response.setHeader("Access-Control-Expose-Headers", "Location");
+		
 		// 目的讓前後端分離時候不會發生response header [last modified time]回傳錯誤
 		if (request.getMethod().equalsIgnoreCase("OPTIONS")) {
 			response.setStatus(HttpServletResponse.SC_OK);
