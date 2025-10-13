7c6597d4a08a0e04df224e55a6e3b9e2ab7c7982
LionFang0620 <yiweifnag@gmail.com>
2025-10-13 22:30:06 +0800

    更新sql資料表
    1.member表，member_id設定為Unique
    
    2.Store表塞入新結構的資料做為測試用

--- database_easybuyfarmForSpringboot/easybuyfarm_member.sql
+++ database_easybuyfarmForSpringboot/easybuyfarm_member.sql
@@ -39,7 +39,8 @@ CREATE TABLE `member` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `phone_UNIQUE` (`phone`),
-  UNIQUE KEY `email` (`email`)
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `member_id_UNIQUE` (`member_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -62,4 +63,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-10-08 18:25:20
+-- Dump completed on 2025-10-13 22:28:05
--- database_easybuyfarmForSpringboot/easybuyfarm_store.sql
+++ database_easybuyfarmForSpringboot/easybuyfarm_store.sql
@@ -27,12 +27,13 @@ DROP TABLE IF EXISTS `store`;
 CREATE TABLE `store` (
   `id` int NOT NULL AUTO_INCREMENT,
   `store_id` varchar(10) NOT NULL,
-  `member_id` varchar(45) NOT NULL,
+  `member_id` varchar(100) NOT NULL,
   `name` varchar(100) NOT NULL,
   `introduce` varchar(255) DEFAULT NULL,
   `store_img` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `store_id_UNIQUE` (`store_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,7 +42,7 @@ CREATE TABLE `store` (
 
 LOCK TABLES `store` WRITE;
 /*!40000 ALTER TABLE `store` DISABLE KEYS */;
-INSERT INTO `store` VALUES (1,'s001','sn202509240001','好水果賣場','水果好吃',NULL),(2,'s002','sn202509240001','蔬菜賣場','蔬菜好吃',NULL),(3,'s003','sn202509240002','神秘綠色葉子','會帶來快樂喔',NULL);
+INSERT INTO `store` VALUES (1,'s001','sn202509240001','禾田','每粒米都用心對待','d90f0529-dbdb-46a9-9057-9239fc31cc24.jpg'),(2,'s002','sn202509240001','蔬菜賣場','蔬菜好吃','02.jpg'),(3,'s003','sn202509240002','神秘綠色葉子','會帶來快樂喔','03.jpg'),(4,'s004','sn202510010002','枕頭山牧場','優質鮮乳的首選商店','faf39388-ef89-4477-8dd1-765d45345016.jpg');
 /*!40000 ALTER TABLE `store` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -54,4 +55,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-10-08 18:25:20
+-- Dump completed on 2025-10-13 22:28:05
