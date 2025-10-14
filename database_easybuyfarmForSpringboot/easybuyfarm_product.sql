1a52a82475e4af066d181e23131247477cf23d5a
LionFang0620 <yiweifnag@gmail.com>
2025-10-14 22:44:17 +0800

    調整資料結構與欄位設定

--- database_easybuyfarmForSpringboot/easybuyfarm_product.sql
+++ database_easybuyfarmForSpringboot/easybuyfarm_product.sql
@@ -26,7 +26,7 @@ DROP TABLE IF EXISTS `product`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `product` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `store_id` varchar(45) DEFAULT NULL,
+  `store_id` varchar(10) DEFAULT NULL,
   `product_id` varchar(45) DEFAULT NULL,
   `name` varchar(100) DEFAULT NULL,
   `price` int DEFAULT NULL,
@@ -44,7 +44,7 @@ CREATE TABLE `product` (
 
 LOCK TABLES `product` WRITE;
 /*!40000 ALTER TABLE `product` DISABLE KEYS */;
-INSERT INTO `product` VALUES (1,'s001','p001','番茄',30,'10','150','這是一顆好吃的番茄','tomto.png'),(2,'s001','p002','柳丁',50,'100','200','這是一顆好吃的柳丁','02.png'),(3,'s001','p003','拔辣',20,'50','400','拔辣巴巴巴巴','03.png'),(4,'s002','p004','高麗菜',190,'10','1200','高麗菜好好吃喔','04.png'),(5,'s002','p005','青江菜',100,'10','150','綠油油的喔','05.png'),(6,'2','p006','好吃番茄',500,'2','500g','好吃啦哪次不好吃了','2f4aef8d-c5c0-4493-b826-b8bf64819524.jpg');
+INSERT INTO `product` VALUES (1,'s001','p001','小黃瓜',150,'20','10g','清脆黃瓜','68d4bd19-14b0-4a60-a1c4-0acbb021824e.jpg'),(2,'s001','p002','柳丁',50,'100','200','這是一顆好吃的柳丁','02.png'),(3,'s001','p003','拔辣',20,'50','400','拔辣巴巴巴巴','03.png'),(4,'s002','p004','高麗菜',190,'10','1200','高麗菜好好吃喔','04.png'),(5,'s002','p005','青江菜',100,'10','150','綠油油的喔','05.png');
 /*!40000 ALTER TABLE `product` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -57,4 +57,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-10-14 17:21:46
+-- Dump completed on 2025-10-14 22:40:14
